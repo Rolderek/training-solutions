75998172b7dd339e9f15cf816d8e53a63866970c
Rolderek <restaslaci@gmail.com>
2020-11-02 19:27:01 +0100

    Változók

---
+++ src/main/java/localvariables/LoacalVariablesMain.java
@@ -0,0 +1,39 @@
+package localvariables;
+
+public class LoacalVariablesMain {
+
+
+    public static void main(String[] args) {
+
+        boolean b;
+
+        b = true;
+
+        //System.out.println(b);
+
+        int a = 2;
+
+        int i = 3;
+
+        int j = 4;
+
+        //System.out.println(k);
+
+        int k = i;
+
+        String s = "Hello Wolrd!";
+
+        String t = s;
+
+        {
+            int x = 0;
+            System.out.println(k);
+        }
+
+        //System.out.println(x);
+
+
+    }
+
+
+}
