0a3ab01fea1fb484ba4d0abbdf392f803afe6b59
Rolderek <restaslaci@gmail.com>
2020-11-02 20:28:20 +0100

    Kifejezések utasítások anyag 2

---
+++ src/main/java/Statemnts/DivByThree.java
@@ -0,0 +1,13 @@
+package Statemnts;
+
+import java.util.Scanner;
+
+public class DivByThree {
+    public static void main(String[] args) {
+        System.out.println("Adj meg egy számot.");
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+
+        System.out.println(i % 3 == 0 ? "Osztható hárommal" : "Nem osztható hárommal" );
+    }
+}
