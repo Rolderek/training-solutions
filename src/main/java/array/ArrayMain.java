704961403db8aa45b3f5a42ac18d0d4b3d2d56f0
Rolderek <restaslaci@gmail.com>
2020-11-03 19:22:25 +0100

    Tömbök a hét napjai házi

---
+++ src/main/java/array/ArrayMain.java
@@ -0,0 +1,11 @@
+package array;
+
+public class ArrayMain {
+    public static void main(String[] args) {
+        String[] dayName = {"Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat", "Vasárnap"};
+
+        System.out.println(dayName[1]);
+        System.out.println(dayName[1]  + ", " + dayName[2]  + ", " + dayName[3]  + ", " + dayName[4]  + ", " + dayName[5]  + ", " + dayName[6]  + ", " + dayName[0]);
+
+    }
+}
