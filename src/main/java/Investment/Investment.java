ef0d55bca7eabe392c7b35dcf221bb14b547ce96
Rolderek <restaslaci@gmail.com>
2020-11-03 17:33:31 +0100

    Investment feladat, csak elkezdve.

---
+++ src/main/java/Investment/Investment.java
@@ -0,0 +1,48 @@
+package Investment;
+
+public class Investment {
+
+    private double cost;
+    private int found;
+    private int interestRate;
+    private boolean active;
+
+    Investment investment = new Investment() {
+        this.cost = cost;
+        this.found = found;
+        this.interestRate = interestRate;
+        this.active = active;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public int getFound() {
+        return found;
+    }
+
+    public void setFound(int found) {
+        this.found = found;
+    }
+
+    public int getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(int interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
---
+++ src/main/java/Investment/InvestmentMain.java
@@ -0,0 +1,4 @@
+package Investment;
+
+public class InvestmentMain {
+}
