8410cd4aa61c9fab2400a5dce63876ba4f6f88f6
Rolderek <restaslaci@gmail.com>
2020-11-07 12:30:44 +0100

    Projekt munka 2020.11.07T12:30

--- src/main/java/week02/Office.java
+++ src/main/java/week02/Office.java
@@ -1,5 +1,25 @@
 package week02;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Office {
 
+    Scanner scanner = new Scanner(System.in);
+
+    private List<String> MeetingRoom = new ArrayList<>();
+
+    public void addMeetingRoom() {
+        System.out.println("Add meg a tárgyaló nevét:");
+        String name = scanner.nextLine();
+        System.out.println("Add meg a tárgyaló hosszát:");
+        int length = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("add meg a tárgyaló szélességét:");
+        int wigth = scanner.nextInt();
+        scanner.nextLine();
+        MeetingRoom meetingRoom = new MeetingRoom();
+
+    }
 }
