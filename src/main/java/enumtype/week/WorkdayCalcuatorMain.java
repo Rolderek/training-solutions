8c57a968cbf955ae8dc570106bef8c94808410ea
Rolderek <restaslaci@gmail.com>
2020-11-11 17:33:13 +0100

    Enum hét napjai

---
+++ src/main/java/enumtype/week/WorkdayCalcuatorMain.java
@@ -0,0 +1,14 @@
+package enumtype.week;
+
+
+
+public class WorkdayCalcuatorMain {
+
+
+
+    public static void main(String[] args) {
+
+        System.out.println(new WorkdayCalculator().dayTypes(Day.CSÜTRÖTÖK, 8));
+    }
+
+}
