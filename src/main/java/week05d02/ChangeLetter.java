1966970cfc8642f23a563fe108c04d9cbedb471b
Rolderek <restaslaci@gmail.com>
2020-11-25 18:28:37 +0100

    Konzultációs feladat 11.24 feltöltve 11.25

--- src/main/java/week05d02/ChangeLetter.java
+++ src/main/java/week05d02/ChangeLetter.java
@@ -1,20 +1,26 @@
 package week05d02;
 
+import schoolrecords.StudyResultByName;
+
 import java.util.Scanner;
 
 public class ChangeLetter {
 
     private static final String VOWELS_TO_CHANGE = "AEIOU";
 
+    private static final String REPLACE = "*";
+
+
     Scanner scanner = new Scanner(System.in);
 
     public static String changeVowels(String workWith) {
         StringBuilder builder = new StringBuilder();
         for (int i = 0; i < workWith.length(); i++) {
-            if (VOWELS_TO_CHANGE.indexOf(workWith.toUpperCase().charAt(i)) > -1) {
-                builder.append("*");
+            String k = workWith.substring(i, i + 1);
+            if (VOWELS_TO_CHANGE.contains(k)) {
+                builder.append(REPLACE);
             } else  {
-                builder.append(workWith.substring(i,i+1));
+                builder.append(k);
             }
 
         }
