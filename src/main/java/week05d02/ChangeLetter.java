bf4fdf66f236b2115630d7215895b22443092841
Rolderek <restaslaci@gmail.com>
2020-11-25 18:21:35 +0100

    Konzultációs feladat 11.24 feltöltve 11.25

--- src/main/java/week05d02/ChangeLetter.java
+++ src/main/java/week05d02/ChangeLetter.java
@@ -9,20 +9,20 @@ public class ChangeLetter {
     Scanner scanner = new Scanner(System.in);
 
     public static String changeVowels(String workWith) {
-        String replace = "";
+        StringBuilder builder = new StringBuilder();
         for (int i = 0; i < workWith.length(); i++) {
             if (VOWELS_TO_CHANGE.indexOf(workWith.toUpperCase().charAt(i)) > -1) {
-                replace += "*";
+                builder.append("*");
             } else  {
-                replace += workWith.charAt(i);
+                builder.append(workWith.substring(i,i+1));
             }
 
         }
-        System.out.println(replace);
-        return replace;
+        System.out.println(builder);
+        return builder.toString();
     }
 
     public static void main(String[] args) {
-        changeVowels("ApacukaFundaluka");
+        changeVowels("ApacukaFundalukacocaloLa");
     }
 }
