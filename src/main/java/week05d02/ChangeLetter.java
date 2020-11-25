ea4962e8807be260c364a23e67a5e758462ede45
Rolderek <restaslaci@gmail.com>
2020-11-25 18:34:51 +0100

    Konzultációs feladat 11.24 feltöltve 11.25

--- src/main/java/week05d02/ChangeLetter.java
+++ src/main/java/week05d02/ChangeLetter.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class ChangeLetter {
 
-    private static final String VOWELS_TO_CHANGE = "AEIOU";
+    private static final String VOWELS_TO_CHANGE = "aeiuo";
 
     private static final String REPLACE = "*";
 
@@ -15,7 +15,7 @@ public class ChangeLetter {
 
     public static String changeVowels(String workWith) {
         StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < workWith.length(); i++) {
+        for (int i = 0; i < workWith.toLowerCase().length(); i++) {
             String k = workWith.substring(i, i + 1);
             if (VOWELS_TO_CHANGE.contains(k)) {
                 builder.append(REPLACE);
@@ -24,11 +24,11 @@ public class ChangeLetter {
             }
 
         }
-        System.out.println(builder);
-        return builder.toString();
+        System.out.println(builder.toString().toLowerCase());
+        return builder.toString().toLowerCase();
     }
 
     public static void main(String[] args) {
-        changeVowels("ApacukaFundalukacocaloLa");
+        changeVowels("apacukafundalukacocalola");
     }
 }
