3225a1633f8a988b377f6d605b9d035c48691349
Rolderek <restaslaci@gmail.com>
2020-12-01 17:52:54 +0100

    12:01 házi

---
+++ src/main/java/week06d02/Store.java
@@ -0,0 +1,18 @@
+package week06d02;
+
+
+
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class Store {
+
+    List<Product> products = new ArrayList<>();
+
+    public int getProductByCategoryByName(List<Product> products, Locale.Category category) {
+
+    }
+}
