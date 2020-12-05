d7487a9e54f31eed5b5a2648cf4baf6cd6179204
Rolderek <restaslaci@gmail.com>
2020-12-05 11:28:06 +0100

    12.05 heti feladatok pótlása, de hármat újra meg kell megcsinálni!!!!!

--- src/main/java/week06d02/Product.java
+++ src/main/java/week06d02/Product.java
@@ -3,12 +3,21 @@ package week06d02;
 public class Product {
 
     private String name;
-    private String category;
+    private Category category;
     private int price;
 
-    public Product(String name, String category, int price) {
+    public Product(String name, Category category, int price) {
+        if (name == null || "".equals(name)) {
+            throw new NullPointerException("A név nem lehet NULL!");
+        }
         this.name = name;
+        if (category == null) {
+            throw new NullPointerException("A kategória nem lehet NULL!");
+        }
         this.category = category;
+        if (1 > price) {
+            throw new IllegalArgumentException("Az ár nem lehet kisebb nullánál vagy egyenlő vele!");
+        }
         this.price = price;
     }
 
@@ -16,11 +25,16 @@ public class Product {
         return name;
     }
 
-    public String getCategory() {
+    public Category getCategory() {
         return category;
     }
 
     public int getPrice() {
         return price;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + category + "), " + price;
     }
 }
--- src/main/java/week06d02/Store.java
+++ src/main/java/week06d02/Store.java
@@ -1,9 +1,5 @@
 package week06d02;
 
-
-
-
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -12,7 +8,29 @@ public class Store {
 
     List<Product> products = new ArrayList<>();
 
-    public int getProductByCategoryByName(List<Product> products, Locale.Category category) {
+    public int getProductByCategoryByName(List<Product> products, Category category) {
+        isNull(products);
+        int i = 0;
+        for (Product product: products) {
+            if (product.getCategory() == category) {
+                i++;
+            }
+        }
+        return i;
+    }
 
+    public void isNull(List<Product> products) {
+        if (products == null || products.size() == 0) {
+            throw new NullPointerException("A terméklista nem lehet üres!");
+        }
+    }
+
+    public List<CategorySum> getProductsByCategory(List<Product> products) {
+        List<CategorySum> categorySums = new ArrayList<>();
+        for (Category category : Category.values()) {
+            int i = getProductByCategoryByName(products, category);
+            categorySums.add(new CategorySum(i, category));
+        }
+        return categorySums;
     }
 }
