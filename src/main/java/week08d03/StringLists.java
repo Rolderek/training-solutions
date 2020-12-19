2de741957b012ecfc5504789c3e93adc9e907094
Rolderek <restaslaci@gmail.com>
2020-12-19 16:43:06 +0100

    12.19 16-ikei házi

---
+++ src/main/java/week08d03/StringLists.java
@@ -0,0 +1,19 @@
+package week08d03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringLists {
+
+    public List<String> stringListsUnion(List<String> first, List<String> second) {
+        List<String> result = new ArrayList<>();
+        result.addAll(first);
+        for (String element : second) {
+            if (!result.contains(element)) {
+                result.add(element);
+            }
+        }
+        return result;
+    }
+    }
+}
