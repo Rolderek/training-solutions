28cb4d11a48bf1a697612227d1567d823627304d
Rolderek <restaslaci@gmail.com>
2020-12-22 11:41:42 +0100

    12.26 11 órás konzultáció

--- src/main/java/week09d02/FibCalculator.java
+++ src/main/java/week09d02/FibCalculator.java
@@ -1,8 +1,38 @@
 package week09d02;
 
+
 public class FibCalculator {
 
-    public long sumEvens(int bound) {
+   static long sumEvens(int bound) {
+       int sum = 0;
+       int fibN = 0;
+       while (fib(fibN) < bound) {
+           if (FIB_NUMS[fibN] % 2 == 0) {
+               sum += FIB_NUMS[fibN];
+           }
+           fibN++;
+       }
+       return sum;
+   }
+
+    private static int[] FIB_NUMS = new int[1000];
+
+    public static void main(String[] args) {
+        System.out.println(fib(8));
+    }
+
+    static int fib(int n) {
+        if (n==0){
+            return 0;
+        } else if (n==1) {
+            return 1;
+        } else {
+            if (FIB_NUMS[n] == 0) {
+            } return fib(n) + fib(n - 2);
+            }
+        }
+    }
+    /*public long sumEvens(int bound) {
         long sum = 0;
         long a = 0;
         long b = 0;
@@ -19,5 +49,7 @@ public class FibCalculator {
         return sum;
     }
 
+
+     */
 
 }
