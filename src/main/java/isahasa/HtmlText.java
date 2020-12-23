756a0384483eda0203950838205e647f42f70c37
Rolderek <restaslaci@gmail.com>
2020-12-23 14:36:17 +0100

    12.23

---
+++ src/main/java/isahasa/HtmlText.java
@@ -0,0 +1,14 @@
+package isahasa;
+
+public class HtmlText implements TextSource {
+
+    private String plainText;
+
+    public HtmlText(String plainText) {
+        this.plainText = plainText;
+    }
+
+    public String getPlainText() {
+        return plainText;
+    }
+}
