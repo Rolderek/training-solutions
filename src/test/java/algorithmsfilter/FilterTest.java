4401a55fc3e99f088e4776c122abdba353cd1164
Rolderek <restaslaci@gmail.com>
2021-01-06 11:39:36 +0100

    01.06 házik pótlása

---
+++ src/test/java/algorithmsfilter/FilterTest.java
@@ -0,0 +1,25 @@
+package algorithmsfilter;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FilterTest {
+
+    @Test
+    void filterByName() {
+        List<Trainer> trainers = new ArrayList<>();
+        trainers.add(new Trainer("Jhon Doe", 1970));
+        trainers.add(new Trainer("Jack Doe", 1970));
+        trainers.add(new Trainer("Jhon Smith", 1970));
+
+        List<Trainer> filteredTrainers = new Filter().filterByName(trainers, "Doe");
+
+        assertEquals(2, filteredTrainers.size());
+        assertEquals("Jhon Doe", filteredTrainers.get(0).getName());
+        assertEquals("Jack Doe", filteredTrainers.get(1).getName());
+    }
+}
