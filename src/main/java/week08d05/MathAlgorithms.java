97326203666a9d01dbbbfb815ce34c17cc25abb5
Rolderek <restaslaci@gmail.com>
2021-01-06 08:22:39 +0100

    01.06 házik pótlása

--- src/main/java/week08d05/MathAlgorithms.java
+++ src/main/java/week08d05/MathAlgorithms.java
@@ -1,8 +1,10 @@
 package week08d05;
 
+import java.util.List;
+
 public class MathAlgorithms {
 
-    public int greatestCommonDivisor(int one, int two) {
+    public int greatestCommonDivisorFirst(int one, int two) {
         if (one < 1 || two < 1) {
             throw new IllegalArgumentException("Nem lehet nulla vagy nagatív.");
         }
@@ -14,5 +16,30 @@ public class MathAlgorithms {
         }
         return div;
     }
+
+    public int greatestCommonDivisorSecond(int a, int b) {
+        int result = 1;
+        for (int i = 2; i <= a && a <= b; i++) {
+            if (a % i == 0 && b % i ==0) {
+                result = i;
+            }
+        }
+            return result;
+    }
+
+    //visszafelé számolva így gyorsabb
+    public int greatestCommonDivisorThird (int a, int b) {
+        int min = a;
+        if (b < min) {
+            min = b;
+        }
+        for (int i = min; i >= 2; i--) {
+            if (a % i == 0 && b % i ==0){
+                return i;
+            }
+        }
+        return 1;
+    }
+
 
 }
