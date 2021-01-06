bb64522ed5435aab3344d37a7e58c4786a819f18
Rolderek <restaslaci@gmail.com>
2021-01-06 08:54:49 +0100

    01.06 házik pótlása

---
+++ src/test/java/week08d05/MathAlgorithmsTest.java
@@ -0,0 +1,33 @@
+package week08d05;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MathAlgorithmsTest {
+
+    @Test
+    void greatestCommonDivisorFirst() {
+        assertEquals(41, new MathAlgorithms().greatestCommonDivisorFirst(41, 82));
+        assertEquals(3, new MathAlgorithms().greatestCommonDivisorFirst(6, 3));
+        assertEquals(1, new MathAlgorithms().greatestCommonDivisorFirst(1, 1));
+    }
+
+    @Test
+    void greatestCommonDivisorSecond() {
+        assertEquals(6, new MathAlgorithms().greatestCommonDivisorSecond(6, 12));
+        assertEquals(3, new MathAlgorithms().greatestCommonDivisorSecond(3, 9));
+    }
+
+    @Test
+    void greatestCommonDivisorThird() {
+        assertEquals(1, new MathAlgorithms().greatestCommonDivisorThird(1, 3));
+        assertEquals(8, new MathAlgorithms().greatestCommonDivisorThird(8, 16));
+    }
+
+    @Test
+    void greatestCommonDivisorFouth() {
+        assertEquals(5, new MathAlgorithms().greatestCommonDivisorFouth(5, 25));
+        assertEquals(2, new MathAlgorithms().greatestCommonDivisorFouth(2, 4));
+    }
+}
