e848c002293ce1cf59f4ce9f210d4deb5df80a40
Rolderek <restaslaci@gmail.com>
2021-01-06 09:24:33 +0100

    01.06 házik pótlása

---
+++ src/main/java/algorithmssum/trainers/Trainer.java
@@ -0,0 +1,21 @@
+package algorithmssum.trainers;
+
+public class Trainer {
+
+    private int age;
+    private String name;
+
+    public Trainer(int age) {
+        this.age = age;
+        this.name =name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
