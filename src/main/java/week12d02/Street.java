ebe166de8857275109bd04d1cc756459c47aaaea
Rolderek <restaslaci@gmail.com>
2021-01-20 19:26:50 +0100

    01.19 napi feladat,

--- src/main/java/week12d02/Street.java
+++ src/main/java/week12d02/Street.java
@@ -7,13 +7,20 @@ public class Street {
 
     private List<Site> soldSites = new ArrayList<>();
 
-/*
+    public List<Site> getSoldSites() {
+        return soldSites;
+    }
+
+    /*
 ide írni a metódus részletet ami végig megy a Site-ok páros/páratlan attribútumain
 és számolja őket majd vissza ad egy listát ami számolja hogy melyik hányszor volt
 két változó a for-ba és az egyik az egyiket a másik a másikat számolja majd minden elemnél kiírni
 az aktuális házszámot annka függvényében hogy mibél hány darabot számolt addig.
-
-
  */
+
+    public List<Site> sellSite(Site site) {
+        soldSites.add(site);
+        return soldSites;
+    }
 
 }
